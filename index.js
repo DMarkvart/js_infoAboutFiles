7cdb207fb4e5c34a502ef6f7db06a74d66d7c61a
Dmitry Markvart <mdmarkwart@gmail.com>
2025-01-12 12:26:50 +0300

    train module CommonJs (add math.js, files.txt)

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-gfhfgh;
-gfhgfhf;
+const {datas, PI} = require("./new_folder/math.js")
+console.log(datas("./new_folder/new_folder/files.txt"));
+console.log(PI);
--- new_folder/math.js
+++ new_folder/math.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const fs = require("fs");
+
+function datas(filePas) {
+   console.log( fs.readFileSync(filePas).toString());
+}
+
+const PI = 3.14;
+
+module.exports = {datas, PI}
